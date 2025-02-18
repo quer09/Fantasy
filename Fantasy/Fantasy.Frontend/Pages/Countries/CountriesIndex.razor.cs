4982542de1c1b1ce9de073467fc6f6215cc28920
quervin <quervin.hidalgo@grupopromerica.com>
2025-02-18 15:36:24 -0600

    Generic List

---
+++ Fantasy/Fantasy.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,17 @@
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Frontend.Pages.Countries;
+
+public partial class CountriesIndex
+{
+    [Inject] private IRepository Repository { get; set; } = null!;
+    private List<Country>? Countries { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var responseHttp = await Repository.GetAsync<List<Country>>("api/v1/Countries");
+        Countries = responseHttp.Response;
+    }
+}
---
+++ Fantasy/Fantasy.Frontend/Repositories/HttpResponseWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+
+namespace Fantasy.Frontend.Repositories;
+
+public class HttpResponseWrapper<T>
+{
+    public HttpResponseWrapper(T? response, bool error, HttpResponseMessage httpResponseMessage)
+    {
+        Response = response;
+        Error = error;
+        HttpResponseMessage = httpResponseMessage;
+    }
+
+    public T? Response { get; }
+    public bool Error { get; }
+    public HttpResponseMessage HttpResponseMessage { get; }
+
+    public async Task<string?> GetErrorMessageAsync()
+    {
+        if (!Error)
+        {
+            return null;
+        }
+
+        switch (HttpResponseMessage.StatusCode)
+        {
+            case HttpStatusCode.NotFound:
+                return "Recurso no encontrado.";
+
+            case HttpStatusCode.BadRequest:
+                return await HttpResponseMessage.Content.ReadAsStringAsync();
+
+            case HttpStatusCode.Unauthorized:
+                return "Tienes que estar logueado para ejecutar esta operación.";
+
+            case HttpStatusCode.Forbidden:
+                return "No tienes permisos para hacer esta operación.";
+
+            default:
+                return "Ha ocurrido un error inesperado.";
+        }
+    }
+}
