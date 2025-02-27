a2ae0aed4f8cb44293e54a0a7b3aff165b1cca7d
quervin <quervin.hidalgo@grupopromerica.com>
2025-02-27 14:26:43 -0600

    Add team entity

--- Fantasy/Fantasy.Backend/Data/DataContext.cs
+++ Fantasy/Fantasy.Backend/Data/DataContext.cs
@@ -10,10 +10,22 @@ public class DataContext : DbContext
     }
 
     public DbSet<Country> Countries { get; set; }
+    public DbSet<Team> Teams { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        modelBuilder.Entity<Team>().HasIndex(x => new { x.CountryId, x.Name }).IsUnique();
+        DisableCasCadingDelete(modelBuilder);
+    }
+
+    private void DisableCasCadingDelete(ModelBuilder modelBuilder)
+    {
+        var releationships = modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys());
+        foreach (var relationship in releationships)
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
     }
 }
